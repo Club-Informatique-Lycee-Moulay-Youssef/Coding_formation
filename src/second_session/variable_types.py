9a412808507002dc13a2a0a2023ed4c7365eb1d4
Walker <80550093+Walker974@users.noreply.github.com>
2022-10-14 22:20:59 +0100

    Variables Types

---
+++ src/second_session/variable_types.py
@@ -0,0 +1,12 @@
+# String (Chaine de caractères)
+first_type = "moulayoussef@gmail.com"
+# Integer (Entier)
+second_type = 10
+# Float (Nombre à virgule)
+third_type = 10.5
+# Boolean (Vrai ou Faux)
+fourth_type = True
+fourth_type2 = False
+# Array (Tableau)
+fifth_type = [1, 2, 3, 4, 5]
+fifth_type2 = ["Walker", "birch", "Treifa", "Adam"]
